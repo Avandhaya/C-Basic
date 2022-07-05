587fdbecea0cf7b0cba5fc777b1c3d8317b22389
Vihanga Avandhaya <87765555+Avandhaya@users.noreply.github.com>
2022-07-05 20:16:41 +0530

    Update Playing With Characters (Q2).c

--- RackerRank Questions of C language/Playing With Characters (Q2).c
+++ RackerRank Questions of C language/Playing With Characters (Q2).c
@@ -1,12 +1,6 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
 
-*******************************************************************************/
-
-//Question 2
+//Question 2 (Playing With Characters)
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
